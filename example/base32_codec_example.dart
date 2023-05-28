8f32dae33133a2ffabbc65025eb91a9d8292307d
mdh <mdh@bivens.dev>
2023-05-28 17:30:49 +0100

    Blank Dart 3 package

---
+++ example/base32_codec_example.dart
@@ -0,0 +1,6 @@
+import 'package:base32_codec/base32_codec.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/base32_codec_test.dart
@@ -0,0 +1,16 @@
+import 'package:base32_codec/base32_codec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
